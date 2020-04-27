9082b71a689d81d269097bf53ec14482352af3f8
lille-boy <arnaud.pinoit@gmail.com>
2020-04-27 15:51:30 +0200

    Added public function description

--- find_intersection.h
+++ find_intersection.h
@@ -12,7 +12,14 @@
 #define DEBUG 0
 
 /******************************************************************************
- *
+ * Description:
+ * - finds the intersection between 2 arrays.
+ * Parameter:
+ * - input_string contains two arrays. The format is ["array1", "array2"],
+     for example: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+ * Return value:
+ * - returns a pointer to the intersection, a comma-separated string
+ *   containing the numbers found in both arrays
  *****************************************************************************/
 char* find_intersection(char *input_string);
 
