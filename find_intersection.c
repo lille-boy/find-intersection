9aad14ef84d4e1af92676fadfaf2df7e2e15afee
lille-boy <arnaud.pinoit@gmail.com>
2020-04-27 15:51:57 +0200

    Refactored code

--- find_intersection.c
+++ find_intersection.c
@@ -11,8 +11,26 @@
 #include <stdlib.h>
 #include "find_intersection.h"
 
+const char delimiter[2] = "\"";
+
 /******************************************************************************
- *
+ * Function that extracts one array from the input string
+ *****************************************************************************/
+static void extract_array(char *token, char *array)
+{
+    if (token != NULL) {
+        token = strtok(NULL, delimiter);
+        if (token != NULL) {
+            strcpy(array, token);
+            if (DEBUG > 1) {
+                printf("array: %s\n", array);
+            }
+        }
+    }
+}
+
+/******************************************************************************
+ * Function that finds the numbers that appear in both input arrays
  *****************************************************************************/
 char* find_intersection(char *input_string)
 {
@@ -27,48 +45,33 @@ char* find_intersection(char *input_string)
     }
 
     /* Extract first array */
-    const char delimiter[2] = "\"";
     char *token = strtok(input_string, delimiter);
-    if (token != NULL) {
-        token = strtok(NULL, delimiter);
-        if (token != NULL) {
-            strcpy(array1, token);
-            if (DEBUG > 1) {
-                printf("array1: %s\n", array1);
-            }
-        }
-    }
+    extract_array(token, array1);
 
     /* Extract second array */
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        token = strtok(NULL, delimiter);
-        if (token != NULL) {
-            strcpy(array2, token);
-            if (DEBUG > 1) {
-                printf("array2: %s\n", array2);
-            }
-        }
-    }
+    extract_array(token, array2);
 
     /* Find intersection */
-    if ((token = strtok(array2, " ,")) != NULL) {
-        char *occurence = strstr(array1, token);
-        if (occurence != NULL) {
+    if ((token = strtok(array2, ", ")) != NULL) {
+        if (strstr(array1, token) != NULL) {
             strcpy(intersection, token);
         }
-
-        while( (token = strtok(NULL, " ,")) != NULL) {
-            char *occurence = strstr(array1, token);
-            if (occurence != NULL) {
-                strcat(intersection, ",");
-                strcat(intersection, token);
+        while( (token = strtok(NULL, ", ")) != NULL) {
+            if (strstr(array1, token) != NULL) {
+                if (intersection[0] == '\0') {
+                    strcpy(intersection, token);
+                }
+                else {
+                    strcat(intersection, ", ");
+                    strcat(intersection, token);
+                }
             }
         }
     }
 
+    /* No intersection found */
     if (intersection[0] == '\0') {
-        /* No intersection found */
         intersection = "false";
     }
 
